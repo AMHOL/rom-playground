bbbdac0ad556a5ad08c1abed239eaae3f2b4f92d
Andy Holland <andyholland1991@aol.com>
2015-05-19 22:44:49 +0100

    Use input for command

--- lib/core/commands/create_user.rb
+++ lib/core/commands/create_user.rb
@@ -4,6 +4,7 @@ module Core
       register_as :create
       relation :users
       result :one
+      input Transproc(:accept_keys, %i(id name email))
       validator Core::Validators::User
 
       def execute(user)
